5fe9924d67b508643d01d4e8d081b3a585ad5a1b
Dmitry Karataev <dkarataev1990@gmail.com>
2023-04-18 20:53:04 +0300

    first naive version without any workaround

---
+++ TelegramNotifier/TelegramNotifier/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CommandLine;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+using System.Configuration;
+
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+IConfiguration configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+var tgSettingsConfSection = configuration.GetRequiredSection("TelegramBotSettings").Get<TelegramBotSettings>();
+var tgGroupId = new ChatId(tgSettingsConfSection?.GroupId);
+ITelegramBotClient tgBot = new TelegramBotClient(tgSettingsConfSection.BotKey);
+await tgBot.SendTextMessageAsync(tgGroupId, args[0]);
---
+++ TelegramNotifier/TelegramNotifier/TelegramBotSettings.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+internal class TelegramBotSettings
+{
+    public string BotKey { get; set; }
+    public string GroupId { get; set; }
+}
