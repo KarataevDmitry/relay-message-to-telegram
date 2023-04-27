e471442fa8c9b93d38422ffcabb60d3c788fa5a9
Dmitry Karataev <dkarataev1990@gmail.com>
2023-04-27 10:37:24 +0300

    Done

--- TelegramNotifier/TelegramNotifier/Program.cs
+++ TelegramNotifier/TelegramNotifier/Program.cs
@@ -1,11 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Microsoft.Extensions.Configuration;
-using Telegram.Bot;
-using Telegram.Bot.Types;
 
+using TL;
+
+using WTelegram;
+if (args.Length == 0)
+{
+    Console.WriteLine(File.ReadAllText("APP_USAGE.txt"));
+    return;
+}
 IConfiguration configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
-var tgSettingsConfSection = configuration.GetRequiredSection("TelegramBotSettings").Get<TelegramBotSettings>();
-var tgGroupId = new ChatId(tgSettingsConfSection.GroupId);
-ITelegramBotClient tgBot = new TelegramBotClient(tgSettingsConfSection.BotKey);
-await tgBot.SendTextMessageAsync(tgGroupId, args[0]);
+var tgSettingsConfSection = configuration.GetRequiredSection(nameof(TelegramSettings)).Get<TelegramSettings>();
+using var client = new Client(tgSettingsConfSection.API_ID, tgSettingsConfSection.API_HASH, tgSettingsConfSection.SessionPathName);
+await DoLogin(tgSettingsConfSection.AccountPhone);
+if (args.Length == 2)
+{
+    var targetUserName = args[1];
+    var targetMessage = args[0];
+    var result = await client.Contacts_ResolveUsername(targetUserName);
+    await client.SendMessageAsync(result.User, targetMessage);
+}
+async Task DoLogin(string loginInfo) // (add this method to your code)
+{
+    while (client.User == null)
+        switch (await client.Login(loginInfo)) // returns which config is needed to continue login
+        {
+            case "verification_code": Console.Write("Code: "); loginInfo = Console.ReadLine(); break;
+            case "name": loginInfo = "John Doe"; break;    // if sign-up is required (first/last_name)
+            case "password": loginInfo = Console.ReadLine(); break;// if user has enabled 2FA
+            default: loginInfo = null; break;
+        }
+    Console.WriteLine($"We are logged-in as {client.User} (id {client.User.id})");
+}
---
+++ TelegramNotifier/TelegramNotifier/TelegramSettings.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+internal class TelegramSettings
+{
+    public int API_ID { get; set; }
+    public string API_HASH { get; set; }
+    public string AccountPhone { get; set; }
+    public string SessionPathName { get; set; }
+}
